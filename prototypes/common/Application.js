6beb7855a9e9f19a430ddcfce6cfba1bd728ab62
Oz Wolfe <dreadpirate@lifewanted.com>
2012-03-30 10:56:07 -0700

    Added singleton functionality to the Application

--- prototypes/common/Application.js
+++ prototypes/common/Application.js
@@ -2,10 +2,28 @@
 var Riyda = Riyda || {};
 
 Riyda.Application = (function(){
+    var _instance = null;
+
+    /// Constructor for `Application`
+    /// @constructor
+    ///
+    /// @throws {Error} If an instance already exists.
     function Application(){
+        util.assert( _instance == null );
+        _instance = this;
     }
     var ApplicationProto = Application.prototype;
-    
+
+    /// Retrieves the singleton instance of the `Application`.
+    ///
+    /// @throws {Error} If no instance exists yet.
+    ///
+    /// @return {Application} The singleton instance of the application.
+    Application.getSingleton = function(){
+        util.assert( _instance instanceof Application );
+        return _instance;
+    };
+
     /// Initializes the application.
     ///
     /// This creates a new `Server` and sets up other things required for the
@@ -16,6 +34,7 @@ Riyda.Application = (function(){
 
     /// Retrieves the `Application`'s `Server` instance.
     ApplicationProto.getServer = function(){
+        util.assert( this._server instanceof Riyda.Server );
         return this._server;
     };
 
