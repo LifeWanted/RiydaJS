2c27371f282e087cf0379c03cfc0bbaeca996992
Oz Wolfe <dreadpirate@lifewanted.com>
2012-03-30 12:19:51 -0700

    Server will now connect to connectors

--- prototypes/common/Server.js
+++ prototypes/common/Server.js
@@ -3,8 +3,19 @@ var Riyda = Riyda || {};
 
 Riyda.Server = (function(){
     function Server(){
+        this._connections = {};
     }
     var ServerProto = Server.prototype;
+
+    function _receiveMessage( message ){
+        util.assert( this instanceof Server );
+    }
+
+    ServerProto.connect = function( connector ){
+        util.assert( connector instanceof Riyda.Connector );
+        this._connections[ connector.getID() ] = connector;
+        connector.onReceive( _receiveMessage.bind( this ) );
+    };
 
     return Server;
 })();
