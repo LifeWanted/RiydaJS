b30bce8e3cb1f8187e1cff94ca23c6a6dfdcb84f
Oz Wolfe <dreadpirate@lifewanted.com>
2012-03-30 10:55:31 -0700

    Added util.assert

--- prototypes/common/utilities.js
+++ prototypes/common/utilities.js
@@ -1,6 +1,21 @@
 
 var util = {
 
+    /// Asserts that the given value is true.
+    ///
+    /// @throws {Error} If `val` is not true.
+    ///
+    /// @param {boolean} val The value to assert for truth.
+    'assert' : function( val ){
+        if( val ){
+            throw new Error( 'Assertion failure.' );
+        }
+    }
+
+    /// Sets up `derived` to inherit from `base`.
+    ///
+    /// @param {function} base      The base class constructor.
+    /// @param {function} derived   The derived class constructor.
     'inherit' : function( base, derived ){
         derived.prototype = new base();
         derived.prototype.constructor = derived;
