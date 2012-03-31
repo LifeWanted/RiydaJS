f681251adbd18829ff7af32878dc4a975d1564ed
Oz Wolfe <dreadpirate@lifewanted.com>
2012-03-30 19:46:26 -0700

    Added message receiving

--- prototypes/common/Client.js
+++ prototypes/common/Client.js
@@ -1,10 +1,31 @@
 
 var Riyda = Riyda || {};
 
+/// The `Client` class should be subclassed for each specific client task.
+///
+/// Do not forget to call `this._super.call( this )` in the subclass
+/// constructor.
 Riyda.Client = (function(){
+    /// Client constructor.
+    /// @constructor
     function Client(){
+        this._connector = new Riyda.Connector();
+        this._connector.onClientReceive( _receiveMessage.bind( this ) );
     }
     var ClientProto = Client.prototype;
+
+    /// Handles receiving messges from the `Server`.
+    ///
+    /// @param {*}  message The message that came in.
+    function _receiveMessage( message ){
+        util.assert( this instanceof Client );
+        this.messageReceived( message );
+    }
+
+    /// Abstract method for handling new messages.
+    ///
+    /// @param {*}  message The message received.
+    ClientProto.messageReceived = util.abstract( 'Client.messageReceived' );
 
     return Client;
 })();
--- prototypes/common/Server.js
+++ prototypes/common/Server.js
@@ -1,14 +1,21 @@
 
 var Riyda = Riyda || {};
 
+/// The `Server` class should be subclassed 
 Riyda.Server = (function(){
+    /// Server constructor.
+    /// @constructor
     function Server(){
         this._connections = {};
     }
     var ServerProto = Server.prototype;
 
+    /// Handles receiving messges from the `Server`.
+    ///
+    /// @param {*}  message The message that came in.
     function _receiveMessage( message ){
         util.assert( this instanceof Server );
+        this.messageReceived( message );
     }
 
     ServerProto.connect = function( connector ){
@@ -16,6 +23,11 @@ Riyda.Server = (function(){
         this._connections[ connector.getID() ] = connector;
         connector.onReceive( _receiveMessage.bind( this ) );
     };
+
+    /// Abstract method for handling new messages.
+    ///
+    /// @param {*}  message The message received.
+    ServerProto.messageReceived = util.abstract( 'Server.messageReceived' );
 
     return Server;
 })();
