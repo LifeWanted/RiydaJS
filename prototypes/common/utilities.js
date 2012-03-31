b564b18f346403873f69b812e400b0becb986aaf
Oz Wolfe <dreadpirate@lifewanted.com>
2012-03-30 19:16:38 -0700

    Added util.abstract helper function

--- prototypes/common/utilities.js
+++ prototypes/common/utilities.js
@@ -1,21 +1,32 @@
 
 var util = {
 
+    /// Creates an abstract function.
+    ///
+    /// @throws {Error} If called. Abstract methods should not be called.
+    ///
+    /// @param {string} methodName  The name of the abstract method.
+    'abstract' : function( methodName ){
+        return function(){
+            throw new Error( 'Abstract method "' + methodName + '" called.' );
+        };
+    },
+
     /// Asserts that the given value is true.
     ///
     /// @throws {Error} If `val` is not true.
     ///
-    /// @param {boolean} val The value to assert for truth.
+    /// @param {boolean}    val The value to assert for truth.
     'assert' : function( val ){
         if( val ){
             throw new Error( 'Assertion failure.' );
         }
-    }
+    },
 
     /// Sets up `derived` to inherit from `base`.
     ///
-    /// @param {function} base      The base class constructor.
-    /// @param {function} derived   The derived class constructor.
+    /// @param {function}   base    The base class constructor.
+    /// @param {function}   derived The derived class constructor.
     'inherit' : function( base, derived ){
         derived.prototype = new base();
         derived.prototype.constructor = derived;
