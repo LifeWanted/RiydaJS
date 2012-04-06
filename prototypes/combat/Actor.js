5a6f37a07f91eb2673a8b23b4707695e0a213189
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-06 16:49:47 -0700

    Moved actions over to Riyda.Actor

--- prototypes/combat/Actor.js
+++ prototypes/combat/Actor.js
@@ -11,7 +11,6 @@ Combat.Actor = (function(){
             mana    : 100,
             stamina : 100
         };
-        this._actions = {};
     };
     util.inherit( Riyda.Actor, Actor );
     var ActorProto = Actor.prototype;
@@ -35,40 +34,6 @@ Combat.Actor = (function(){
     /// @return {number} The `Actor`'s stamina.
     ActorProto.getStamina = function(){
         return this._status.stamina;
-    };
-
-    /// Adds a new action to the `Actor`'s repitiore.
-    ///
-    /// @param {Combat.Action} action The action to add.
-    ActorProto.addAction = function( action ){
-        util.assert( action instanceof Combat.Action );
-        this._actions[ action.getName() ] = action;
-    };
-
-    /// Fetches the names of all the actions available to this `Actor`.
-    ///
-    /// @return {Array.<string>} An array of action names.
-    ActorProto.getActionNames = function(){
-        return util.getKeys( this._actions );
-    };
-
-    /// Retrieves the named action from the `Actor`'s action list.
-    ///
-    /// @param {string} actionName The name of the action to retrieve.
-    ///
-    /// @return {Combat.Action} The named action.
-    ActorProto.getAction = function( actionName ){
-        util.assert( this._actions[actionName] instanceof Combat.Action );
-        return this._actions[actionName];
-    };
-
-    /// Performs the named action against the specified target.
-    ///
-    /// @param {string} actionName  The name of the action to perform.
-    /// @param {number} targetID    The ID of the target of the action.
-    ActorProto.perform = function( actionName, targetID ){
-        var action = this.getAction( actionName );
-        var target = Riyda.Application.getSingleton().getActor( targetID );
     };
 
     return Actor;
--- prototypes/common/Actor.js
+++ prototypes/common/Actor.js
@@ -2,13 +2,56 @@ var Riyda = Riyda || {};
 
 Riyda.Actor = (function(){
     function Actor(){
-        this._id = util.generateID();
     }
+    
+    Actor._super = function(){
+        this._id        = util.generateID();
+        this._actions   = {};
+        Riyda.Application.getSingleton().addActor( this );
+    };
     var ActorProto = Actor.prototype;
 
+    /// Retrieves the ID of the `Actor`.
+    ///
+    /// @return {string} The `Actor`'s ID.
     ActorProto.getID = function(){
         return this._id;
     };
 
+    /// Adds a new action to the `Actor`'s repitiore.
+    ///
+    /// @param {Combat.Action} action The action to add.
+    ActorProto.addAction = function( action ){
+        util.assert( action instanceof Combat.Action );
+        this._actions[ action.getName() ] = action;
+    };
+
+    /// Fetches the names of all the actions available to this `Actor`.
+    ///
+    /// @return {Array.<string>} An array of action names.
+    ActorProto.getActionNames = function(){
+        return util.getKeys( this._actions );
+    };
+
+    /// Retrieves the named action from the `Actor`'s action list.
+    ///
+    /// @param {string} actionName The name of the action to retrieve.
+    ///
+    /// @return {Combat.Action} The named action.
+    ActorProto.getAction = function( actionName ){
+        util.assert( this._actions[actionName] instanceof Combat.Action );
+        return this._actions[actionName];
+    };
+
+    /// Performs the named action against the specified target.
+    ///
+    /// @param {string} actionName  The name of the action to perform.
+    /// @param {string} targetID    The ID of the target of the action.
+    ActorProto.perform = function( actionName, targetID ){
+        var action = this.getAction( actionName );
+        var target = Riyda.Application.getSingleton().getActor( targetID );
+    };
+
     return Actor;
 })();
+
