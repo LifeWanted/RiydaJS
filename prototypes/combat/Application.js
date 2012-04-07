119754b45d9ea283ece4a9e294f3d25a55aaf7b4
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-06 22:06:52 -0700

    Added application and subclass clients

---
+++ prototypes/combat/Application.js
@@ -0,0 +1,21 @@
+
+// External variables.
+var Combat  = Combat    || {};
+var Riyda   = Riyda     || null;
+var util    = util      || null;
+
+Combat.Application = (function(){
+    function Application(){
+    }
+    util.instance( Riyda.Application, Application );
+    var ApplicationProto = Application.prototype;
+    
+    ApplicationProto.init = function(){
+        Riyda.Application.prototype.init.call( this );
+        
+        this._playerClient  = new Combat.PlayerClient();
+        this._aiClient      = new Combat.AIClient();
+    };
+    
+    return Application;
+})();
