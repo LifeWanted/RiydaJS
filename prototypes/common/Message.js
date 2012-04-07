c2258d1836918397a0b05fba52c5a24d8a281a8c
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-06 22:59:21 -0700

    Added abstract getType method

--- prototypes/common/Message.js
+++ prototypes/common/Message.js
@@ -1,11 +1,15 @@
 
 // External variables.
-var Riyda = Riyda || {};
+var Riyda   = Riyda || {};
+var util    = util  || null;
 
 Riyda.Message = (function(){
     function Message(){
     }
     var MessageProto = Message.prototype;
+
+    /// Abstract method for getting the type of a message.
+    MessageProto.getType = util.abstract( "Message.getType" );
 
     return Message;
 })();
