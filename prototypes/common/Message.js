29b483b18c939b44b8619be28cb6e70a60888cfd
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-06 22:23:57 -0700

    Adding message base class

---
+++ prototypes/common/Message.js
@@ -0,0 +1,11 @@
+
+// External variables.
+var Riyda = Riyda || {};
+
+Riyda.Message = (function(){
+    function Message(){
+    }
+    var MessageProto = Message.prototype;
+
+    return Message;
+})();
