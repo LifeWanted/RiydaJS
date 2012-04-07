dbb1c4f1c3487989c017271ec0a26a8d79b92294
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-06 22:59:10 -0700

    Added basic handling for NextTurn message

--- prototypes/combat/AIClient.js
+++ prototypes/combat/AIClient.js
@@ -1,16 +1,32 @@
 
 // External variables.
 var Combat  = Combat    || {};
+var Riyda   = Riyda     || null;
 var util    = util      || null;
 
 Combat.AIClient = (function(){
     function AIClient(){
-        this._monster = new Combat.Monster();
+        this._monster   = new Combat.Monster( this );
+        this._handlers  = {
+            Combat.Message.Type.NextTurn : _nextTurn
+        };
     }
     util.inherit( Combat.Client, AIClient );
     var AIClientProto = AIClient.prototype;
 
+    /// Handles a new turn starting.
+    function _nextTurn(){
+        util.assert.instance( this, AIClient );
+        
+        // The AI just does a plain attack every turn.
+        this._monster.attack( "bite" );
+    }
+
+    /// Handles new messages from the server.
     AIClientProto.messageReceived = function( message ){
+        util.assert.instance( message, Combat.Message );
+        util.assert.isFunction( this._handlers[ message.getType() ] );
+        this._handlers[ message.getType() ].call( this, message );
     };
 
     return AIClient;
