23717f57e2707992e315c337efed68dd2bb65e79
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-07 21:16:03 -0700

    Added PerformAction message

--- prototypes/combat/Message.js
+++ prototypes/combat/Message.js
@@ -6,6 +6,10 @@ var util    = util      || null;
 
 Combat.Message = (function(){
     function Message(){}
+    
+    /// @constructor
+    ///
+    /// @param {Message.Type} type The type of this message.
     Message._init = function( type ){
         util.assert( type );
         this._super();
@@ -13,13 +17,44 @@ Combat.Message = (function(){
     };
     var MessageProto = Message.prototype;
 
-    Message.Type = {
-        NextTurn : 'NextTurn'
-    };
+    Message.Type = util.makeEnum([
+        'PerformAction',
+        'ActionResolved'
+    ]);
 
     MessageProto.getType = function(){
         return this._type;
     };
 
     return util.inherit( Riyda.Message, Message );
+})();
+
+/// The `PerformAction` message is sent from the `Client` to the `Server` and indicates 
+Combat.Message.PerformAction = (function(){
+    function PerformAction(){}
+
+    /// @constructor
+    ///
+    /// @param {string}         actorID The ID of the actor to perform the action.
+    /// @param {Combat.Action}  action  The action for the receiver to perform.
+    PerformAction._init = function( actorID, action ){
+        util.assert( actorID );
+        util.assert.instance( action, Combat.Action );
+        this._super( Combat.Message.Type.PerformAction );
+        this._actorID   = actorID;
+        this._action    = action;
+    };
+    var PerformActionProto = PerformAction.prototype;
+
+    /// Retrieves the actor ID associated with this message.
+    ///
+    /// @return {string} The ID of the actor performing the action.
+    util.accessor( PerformActionProto, 'getActorID', '_actorID' );
+
+    /// Retrieves the action associated with this message.
+    ///
+    /// @return {Combat.Action} The action to be performed.
+    util.accessor( PerformActionProto, 'getAction', '_action' );
+
+    return util.inherit( Combat.Message, PerformAction );
 })();
