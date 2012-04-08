57abc72ded477c0473a813ae8ee4622e5509cff0
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-08 10:46:08 -0700

    Added missing super call in constructor

--- prototypes/combat/AIClient.js
+++ prototypes/combat/AIClient.js
@@ -5,9 +5,9 @@ var Riyda   = Riyda     || null;
 var util    = util      || null;
 
 Combat.AIClient = (function(){
-    function AIClient(){
-    }
+    function AIClient(){}
     AIClient._init = function(){
+        this._super();
         this._monster   = new Combat.Monster( this );
         this._handlers  = {};
         this._handlers[Combat.Message.Type.NextTurn] = _nextTurn;
@@ -17,9 +17,9 @@ Combat.AIClient = (function(){
     /// Handles a new turn starting.
     function _nextTurn(){
         util.assert.instance( this, AIClient );
-        
+
         // The AI just does a plain attack every turn.
-        this._monster.attack( "bite" );
+        this._monster.attack( 'bite' );
     }
 
     /// Handles new messages from the server.
