9a1d44667a35a6b7f56474cb936f8b79864f109e
Oz Wolfe <dreadpirate@lifewanted.com>
2012-04-11 19:32:04 -0700

    Added handler for the ActorStatusUpdate message

--- prototypes/combat/AIClient.js
+++ prototypes/combat/AIClient.js
@@ -10,16 +10,35 @@ Combat.AIClient = (function(){
         this._super();
         this._monster   = new Combat.Monster( this );
         this._handlers  = {};
-        this._handlers[Combat.Message.Type.NextTurn] = _nextTurn;
+        this._handlers[Combat.Message.Type.ActionResolved]      = _message_actionResolved;
+        this._handlers[Combat.Message.Type.ActorStatusUpdate]   = _message_actorStatusUpdate;
     };
     var AIClientProto = AIClient.prototype;
 
-    /// Handles a new turn starting.
-    function _nextTurn(){
+    /// Handles the resolution of an action.
+    function _message_actionResolved(){
         util.assert.instance( this, AIClient );
 
         // The AI just does a plain attack every turn.
         this._monster.attack( 'bite' );
+    }
+
+    /// Handles updating the monster's status.
+    function _message_actorStatusUpdate( message ){
+        util.assert.instance( this, AIClient );
+        util.assert.instance( message, Combat.Message.ActorStatusUpdate );
+
+        // We only care about our actor.
+        //
+        // NOTE:    Real clients will need to update their internal status for _all_ actors. For
+        //          this prototype both clients and the server are working on the same state listing
+        //          for the actors.
+        if( this._monster.getID() != message.getActorID() ){
+            return;
+        }
+        
+        // This is an update for our actor, so tell it to update its status.
+        this._monster.updateStatus( message.getStatusUpdate() );
     }
 
     /// Handles new messages from the server.
